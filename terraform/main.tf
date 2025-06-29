fd5b58304e1d32dd0975f5e649a718ae5fb8a6b8
Taha jamil <tahamian37@gmail.com>
2025-06-29 17:12:54 +1000

    terraform ecs module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,69 @@
+
+
+data "aws_vpc" "default" {
+  default = true  
+}
+
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  from_port         = 8000
+  ip_protocol       = "tcp"
+  to_port           = 8000
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" 
+}
+resource "aws_ecs_cluster" "main" {
+  name = "django-cluster"
+}
+resource "aws_iam_role" "ecs_task_execution_role" {
+ name = "django-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+         Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "django-container"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecr_repository" "django_app" {
   name                 = "django-app"
   image_tag_mutability = "MUTABLE"
@@ -5,4 +71,52 @@ resource "aws_ecr_repository" "django_app" {
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = "django-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "django-app"
+      image     = "${aws_ecr_repository.django_app.repository_url}:1"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+        }
+      ]
+    }
+  ])
+}
+
+
+
+
+
+resource "aws_ecs_service" "django" {
+  name            = "django-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+ depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_role_policy]
+
+
+  network_configuration {
+    subnets         = data.aws_subnets.default.ids
+  security_groups = [aws_security_group.allow_tls.id]
+    assign_public_ip = true  
+  }
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.django_app.repository_url
 }
